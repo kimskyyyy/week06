fa1831d632de0046dbb651eb5ac279957c07651a
kimskyyyy <hhhhh823@naver.com>
2022-09-04 21:03:39 +0900

    PostRepository생성

---
+++ src/main/java/com/sparta/week06/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.week06.repository;
+
+import com.sparta.week06.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> { // JpaRepository<엔티티 타입, 식별자 타입>를 상속받아서 기능 구현
+    List<Post> findAllByOrderByModifiedAtDesc(); // 수정일자 내림차순으로 Post List 조회
+}

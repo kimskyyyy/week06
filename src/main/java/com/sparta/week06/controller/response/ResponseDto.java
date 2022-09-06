5d929e9612283b66c39d6f55762b0497384d35f4
kimskyyyy <hhhhh823@naver.com>
2022-09-07 00:32:11 +0900

    responseDto 주석 처리

--- src/main/java/com/sparta/week06/controller/response/ResponseDto.java
+++ src/main/java/com/sparta/week06/controller/response/ResponseDto.java
@@ -3,8 +3,8 @@ package com.sparta.week06.controller.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
+@Getter // Get메소드 일괄 생성
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 만듦
 public class ResponseDto<T> {
     private boolean success;
     private T data;
@@ -18,8 +18,8 @@ public class ResponseDto<T> {
         return new ResponseDto<>(false, null, new Error(code, message));
     }
 
-    @Getter
-    @AllArgsConstructor
+    @Getter // Get메소드 일괄 생성
+    @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 만듦
     static class Error {
         private String code;
         private String message;

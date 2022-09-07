d504006ee697faecee2c0e140b9fdcdb8ea22aef
kimskyyyy <hhhhh823@naver.com>
2022-09-08 02:35:47 +0900

    cors 관련 헤더 추가

--- src/main/java/com/sparta/week06/configuration/WebConfig.java
+++ src/main/java/com/sparta/week06/configuration/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT","DELETE","HEAD")
+                .allowedMethods("GET", "POST", "PUT","DELETE","HEAD","OPTIONS")
                 .exposedHeaders("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)

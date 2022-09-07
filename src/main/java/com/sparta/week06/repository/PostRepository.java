80b006b1f6e0620b02972dfff25f82793c1af669
kimskyyyy <hhhhh823@naver.com>
2022-09-08 01:13:30 +0900

    주석 수정

--- src/main/java/com/sparta/week06/repository/PostRepository.java
+++ src/main/java/com/sparta/week06/repository/PostRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> { // JpaRepository<엔티티 타입, 식별자 타입>를 상속받아서 기능 구현
 // JpaRepository<엔티티 타입, 식별자 타입>를 상속받아서 기능 구현
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc(); // 수정일자 내림차순으로 Post List 조회
